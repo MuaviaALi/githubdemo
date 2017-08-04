d880f657d442c8fa49f1efc8c095a4e6346d3c04
MuaviaALi <30710255+MuaviaALi@users.noreply.github.com>
2017-08-04 07:48:05 -0700

    first commit.

---
+++ TR_api/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TR_api.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Home/TicketBooking")]
+        public bool TicketBooking(int id, string date, string time,string filmName,int seatNumber,int seatId,int price)
+        {
+            return true;
+        }
+
+
+        [HttpGet]
+        [Route("api/Home/TimeTable")]
+        public bool TimeTable(int id, string date, string time, string film, int price)
+        {
+            return true;
+        }
+
+         [HttpGet]
+         [Route("api/Home/SeatsAvailability")]
+        public bool SeatsAvailability(int id, string film, string date, string time, string seats)
+        {
+            return true;
+        }
+        
+    }
+}
+    
+
---
+++ TR_api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TR_api.WebApiApplication" Language="C#" %>
